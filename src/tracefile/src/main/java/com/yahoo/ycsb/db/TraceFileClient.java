3cddabab2e407d1053a67edea051e7882126eca5
Kinan Dak Albab <kinan.bab.14@gmail.com>
2021-04-23 10:22:32 -0400

    Use memory engine

--- src/tracefile/src/main/java/com/yahoo/ycsb/db/TraceFileClient.java
+++ src/tracefile/src/main/java/com/yahoo/ycsb/db/TraceFileClient.java
@@ -59,7 +59,7 @@ public class TraceFileClient extends DB {
       "CREATE TABLE usertable(YCSB_KEY VARCHAR(100) PRIMARY KEY,"
       + " `DEC` VARCHAR(100), USR VARCHAR(100), SRC VARCHAR(100), OBJ VARCHAR(100),"
       + " CAT VARCHAR(100), ACL VARCHAR(100), Data VARCHAR(100), PUR VARCHAR(100),"
-      + " SHR VARCHAR(100), TTL VARCHAR(100));"
+      + " SHR VARCHAR(100), TTL VARCHAR(100)) ENGINE MEMORY;"
       + "\n"
       + "CREATE INDEX pur_index ON usertable(PUR);\n";
 
