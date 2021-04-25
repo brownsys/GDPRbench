293c466a07827c67b9b676c358b0f9131a67d45f
Kinan Dak Albab <kinan.bab.14@gmail.com>
2021-04-25 11:05:44 -0400

    Add traces file, make run-tracefile take the load suffix as an argument

--- src/run-tracefile.sh
+++ src/run-tracefile.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-mvn package
+# Usage: ./run-tracefile <suffix>
+#        such that workloads/pelton_<suffix> is a load config file.
+# Outputs: two trace files for pelton and vanilla MySQL respectively at:
+#          traces/pelton_<suffix>.sql and traces/mysql_<suffix>.sql
 
-./bin/ycsb load tracefile -s -P workloads/gdpr_all -p sharded.path=traces/pelton.sql -p unsharded.path=traces/mysql.sql -p file.append=no
-./bin/ycsb run tracefile -s -P workloads/gdpr_all -p sharded.path=traces/pelton.sql -p unsharded.path=traces/mysql.sql -p file.append=yes
+echo "Don't forget to run 'mvn package' after changes!"
+
+LOAD_CONF="workloads/pelton_$1"
+SFILE_NAME="traces/pelton_$1.sql"
+UFILE_NAME="traces/mysql_$1.sql"
+
+
+./bin/ycsb load tracefile -s -P $LOAD_CONF -p sharded.path=$SFILE_NAME \
+                          -p unsharded.path=$UFILE_NAME -p file.append=no
+
+./bin/ycsb run tracefile -s -P $LOAD_CONF -p sharded.path=$SFILE_NAME \
+                         -p unsharded.path=$UFILE_NAME -p file.append=yes
