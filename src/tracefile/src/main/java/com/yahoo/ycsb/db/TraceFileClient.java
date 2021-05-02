740e1034f67c84e0399c7e4955dafc3f84061a04
Kinan Dak Albab <kinan.bab.14@gmail.com>
2021-05-02 11:12:52 -0400

    Rocksdb loads

--- src/tracefile/src/main/java/com/yahoo/ycsb/db/TraceFileClient.java
+++ src/tracefile/src/main/java/com/yahoo/ycsb/db/TraceFileClient.java
@@ -59,8 +59,9 @@ public class TraceFileClient extends DB {
       "CREATE TABLE usertable(YCSB_KEY VARCHAR(100) PRIMARY KEY,"
       + " `DEC` VARCHAR(100), USR VARCHAR(100), SRC VARCHAR(100), OBJ VARCHAR(100),"
       + " CAT VARCHAR(100), ACL VARCHAR(100), Data VARCHAR(100), PUR VARCHAR(100),"
-      + " SHR VARCHAR(100), TTL VARCHAR(100)) ENGINE MEMORY;\n"
-      + "CREATE INDEX usr_index ON usertable(USR);";
+      + " SHR VARCHAR(100), TTL VARCHAR(100)) ENGINE ROCKSDB;\n"
+      + "CREATE INDEX usr_index ON usertable(USR);\n"
+      + "CREATE INDEX pur_index ON usertable(PUR);\n";
 
   public static final String SHARDED_SCHEMA =
       "CREATE TABLE main(PII_usr VARCHAR(100) PRIMARY KEY);\n"
